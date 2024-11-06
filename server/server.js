ebf22340496ceb1f892504cd231318b3c9218922
GSMFURQAN <gsmfurqan@gmail.com>
2024-11-06 21:02:28 +0530

    deployment step 2

--- server/server.js
+++ server/server.js
@@ -65,12 +65,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
   // if we're in production serve build/index.html file on local server
-//  if(process.env.NODE_ENV == 'production'){
+ if(process.env.NODE_ENV == 'production'){
   app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
-//  }
+ }
 
   // Start the Express server
   app.listen(port, () => {
